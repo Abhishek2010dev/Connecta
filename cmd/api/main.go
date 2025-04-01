6fbe1f44b6a32ff2c4f575d921a7f4ef2d5e145f
Abhishek2010DevSingh <Abhishekkumar2010dev@gmail.com>
2025-04-01 15:09:06 +0530

    main function done

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,4 +1,15 @@
 package main
 
+import (
+	"log"
+
+	"github.com/Abhishek2010dev/Connecta/internal/server"
+	"github.com/Abhishek2010dev/Connecta/pkg/config"
+)
+
 func main() {
+	server := server.New(config.Load())
+	if err := server.ListenAndServe(); err != nil {
+		log.Fatal("Failed to started server:", err)
+	}
 }
