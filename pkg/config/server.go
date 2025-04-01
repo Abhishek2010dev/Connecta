82ad3a45a8694b0d8062a3510529409aca890e89
Abhishek2010DevSingh <Abhishekkumar2010dev@gmail.com>
2025-04-01 19:47:24 +0530

    go style

--- pkg/config/server.go
+++ pkg/config/server.go
@@ -5,19 +5,19 @@ import (
 	"time"
 )
 
-type ServerConfig struct {
+type Server struct {
 	Host    string
 	Port    string
 	Timeout time.Duration
 }
 
-func NewServerConfig() ServerConfig {
+func NewServerConfig() Server {
 	timeout, err := time.ParseDuration(getEnv("SERVER_TIMEOUT"))
 	if err != nil {
 		log.Fatal("Failed to parse timeout")
 	}
 
-	return ServerConfig{
+	return Server{
 		Host:    getEnv("SERVER_HOST"),
 		Port:    getEnv("SERVER_PORT"),
 		Timeout: timeout,
