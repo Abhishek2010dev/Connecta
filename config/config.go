ce0889d9a01efdb716c6fa2ae0dc085aeb4a0316
Abhishek2010DevSingh <Abhishekkumar2010dev@gmail.com>
2025-04-01 13:18:18 +0530

    getEnv function

--- config/config.go
+++ config/config.go
@@ -1,10 +1,25 @@
 package config
 
-type DatabaseConfig struct {
-	URL string
-}
+import (
+	"log"
+	"os"
+)
 
 type ServerConfig struct {
 	Host string
 	Port string
+}
+
+type Config struct {
+	server ServerConfig
+}
+
+func getEnv(key string) {
+	value, exits := os.LookupEnv(key)
+	if value == "" || !exits {
+		log.Fatalf("Error: Missing environment variable %s", key)
+	}
+}
+
+func Load() Config {
 }
