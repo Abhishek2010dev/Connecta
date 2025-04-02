d14c281d992c755db674c633cad84e83c856d966
Abhishek2010DevSingh <Abhishekkumar2010dev@gmail.com>
2025-04-02 15:53:38 +0530

    config: new field for config.Database

--- pkg/config/database.go
+++ pkg/config/database.go
@@ -12,6 +12,10 @@ type Database struct {
 
 func NewDatabase() Database {
 	return Database{
-		URL: getEnv("DATABASE_URL"),
+		URL:             getEnv("DB_URL"),
+		MaxOpenConns:    getIntEnv("DB_MAX_OPEN_CONN"),
+		MaxIdleConns:    getIntEnv("DB_MAX_IDLE_CONN"),
+		ConnMaxLifetime: getDurationEnv("DB_CONN_MAX_LIFETIME"),
+		ConnMaxIdleTime: getDurationEnv("DB_CONN_MAX_IDLE_TIME"),
 	}
 }
