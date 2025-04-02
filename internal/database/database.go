4652bd2bf86582c923225d2bf59b82688afd4b7f
Abhishek2010DevSingh <Abhishekkumar2010dev@gmail.com>
2025-04-02 17:26:23 +0530

    postgresqlDatabase struct new functionm

---
+++ internal/database/database.go
@@ -0,0 +1,30 @@
+package database
+
+import (
+	"database/sql"
+	"fmt"
+
+	"github.com/Abhishek2010dev/Connecta/pkg/config"
+)
+
+type postgresqlDatabase struct {
+	db *sql.DB
+}
+
+func New(cfg config.Database) (*DBProvider, error) {
+	db, err := sql.Open("postgres", cfg.URL)
+	if err != nil {
+		return nil, fmt.Errorf("Failed to create connection: %w", err)
+	}
+
+	db.SetConnMaxIdleTime(cfg.ConnMaxIdleTime)
+	db.SetConnMaxLifetime(cfg.ConnMaxLifetime)
+	db.SetMaxIdleConns(cfg.MaxIdleConns)
+	db.SetMaxOpenConns(cfg.MaxOpenConns)
+
+	if err := db.Ping(); err != nil {
+		return nil, fmt.Errorf("Failed to ping connection: %w", err)
+	}
+
+	return &postgresqlDatabase{db: db}, nil
+}
