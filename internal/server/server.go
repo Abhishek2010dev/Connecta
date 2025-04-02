75ba360967230a32a57b43e29653d6e1b6f9d31a
Abhishek2010DevSingh <Abhishekkumar2010dev@gmail.com>
2025-04-02 18:32:45 +0530

    server: database field

--- internal/server/server.go
+++ internal/server/server.go
@@ -1,16 +1,30 @@
 package server
 
 import (
+	"database/sql"
 	"fmt"
+	"log"
 	"net/http"
 
+	"github.com/Abhishek2010dev/Connecta/internal/database"
 	"github.com/Abhishek2010dev/Connecta/pkg/config"
 )
 
-type Server struct{}
+type Server struct {
+	db *sql.DB
+}
 
 func New(cfg config.Config) *http.Server {
-	NewServer := Server{}
+	database, err := database.New(cfg.Database)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer database.Close()
+
+	NewServer := Server{
+		db: database.Get(),
+	}
+
 	server := &http.Server{
 		Addr:         fmt.Sprintf("%s:%s", cfg.Server.Host, cfg.Server.Port),
 		Handler:      NewServer.RegisterRoutes(),
