d44e510fa592a2705cef2982e44f80389dbf21b6
Abhishek2010DevSingh <Abhishekkumar2010dev@gmail.com>
2025-04-03 15:28:32 +0530

    repository: NewUser function

--- internal/repository/user.go
+++ internal/repository/user.go
@@ -1,12 +1,34 @@
 package repository
 
 import (
+	"database/sql"
+
 	"github.com/Abhishek2010dev/Connecta/internal/dto"
 	"github.com/Abhishek2010dev/Connecta/internal/models"
 )
 
-type UserRespoitory interface {
+type UserRepository interface {
 	Create(payload dto.CreateUserPayload) (*models.User, error)
 	ExitsByEmail(email string) (bool, error)
 	FindByEmail(email string) (*models.User, error)
+}
+
+type userRepositoryImpl struct {
+	db *sql.DB
+}
+
+func NewUser(db *sql.DB) UserRepository {
+	return &userRepositoryImpl{db}
+}
+
+func (u *userRepositoryImpl) Create(payload dto.CreateUserPayload) (*models.User, error) {
+	panic("not implemented") // TODO: Implement
+}
+
+func (u *userRepositoryImpl) ExitsByEmail(email string) (bool, error) {
+	panic("not implemented") // TODO: Implement
+}
+
+func (u *userRepositoryImpl) FindByEmail(email string) (*models.User, error) {
+	panic("not implemented") // TODO: Implement
 }
