27453b46b392da330cd61059f5d392c0335bd37d
Abhishek2010DevSingh <Abhishekkumar2010dev@gmail.com>
2025-04-03 15:22:38 +0530

    dto: user repository impl

--- internal/repository/user.go
+++ internal/repository/user.go
@@ -1,1 +1,12 @@
 package repository
+
+import (
+	"github.com/Abhishek2010dev/Connecta/internal/dto"
+	"github.com/Abhishek2010dev/Connecta/internal/models"
+)
+
+type UserRespoitory interface {
+	Create(payload dto.CreateUserPayload) (*models.User, error)
+	ExitsByEmail(email string) (bool, error)
+	FindByEmail(email string) (*models.User, error)
+}
