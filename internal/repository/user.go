a87dc6652669262fabccf3e4ad17bebc0d5b50d9
Abhishek2010DevSingh <Abhishekkumar2010dev@gmail.com>
2025-04-03 19:44:13 +0530

    repository/user: errors.Is function

--- internal/repository/user.go
+++ internal/repository/user.go
@@ -2,6 +2,7 @@ package repository
 
 import (
 	"database/sql"
+	"errors"
 	"fmt"
 
 	"github.com/Abhishek2010dev/Connecta/internal/dto"
@@ -30,7 +31,7 @@ func (u *userRepositoryImpl) Create(payload dto.CreateUserPayload) (int64, error
 	var userId int64
 	err := u.db.QueryRow(query, payload.Name, payload.Username, payload.Email, payload.Password).Scan(&userId)
 	if err != nil {
-		if err == sql.ErrNoRows {
+		if errors.Is(err, sql.ErrNoRows) {
 			return 0, fmt.Errorf("Failed to create user: %w", err)
 		}
 		return 0, fmt.Errorf("Failed to scan userId: %w", err)
@@ -43,7 +44,7 @@ func (u *userRepositoryImpl) ExitsByEmail(email string) (bool, error) {
 	query := "SELECT 1 FROM users WHERE email = $1"
 	var exits int
 	if err := u.db.QueryRow(query, email).Scan(&exits); err != nil {
-		if err == sql.ErrNoRows {
+		if errors.Is(err, sql.ErrNoRows) {
 			return false, nil
 		}
 		return false, fmt.Errorf("Failed to check email existence: %w", err)
@@ -58,7 +59,7 @@ func (u *userRepositoryImpl) FindByEmail(email string) (*models.User, error) {
 	row := u.db.QueryRow(query, email)
 	err := row.Scan(&user.Id, &user.Name, &user.Username, &user.Email, &user.Password, &user.CreatedAt)
 	if err != nil {
-		if err == sql.ErrNoRows {
+		if errors.Is(err, sql.ErrNoRows) {
 			return nil, fmt.Errorf("User does not exits: %w", err)
 		}
 		return nil, fmt.Errorf("Failed to find user by email %s: %w", email, err)
