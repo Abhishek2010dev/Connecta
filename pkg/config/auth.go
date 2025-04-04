5ad8042292444176f24a2ec249459ced8e79f9d3
Abhishek2010DevSingh <Abhishekkumar2010dev@gmail.com>
2025-04-04 18:41:49 +0530

    auth: removing all field adding new

--- pkg/config/auth.go
+++ pkg/config/auth.go
@@ -1,13 +1,11 @@
 package config
 
 type Auth struct {
-	AccessSecret  string
-	RefreshSecret string
+	JwtSecret string
 }
 
 func NewAuth() Auth {
 	return Auth{
-		AccessSecret:  LoadEnv("AUTH_ACCESS_SECRET"),
-		RefreshSecret: LoadEnv("AUTH_REFRESH_TOKEN"),
+		JwtSecret: LoadEnv("Auth_Jwt_Secret"),
 	}
 }
