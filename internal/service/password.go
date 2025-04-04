6b33755def01ddf6d6648cec998e215de758df82
Abhishek2010DevSingh <Abhishekkumar2010dev@gmail.com>
2025-04-04 18:57:17 +0530

    service: HashPassword Method

--- internal/service/password.go
+++ internal/service/password.go
@@ -1,5 +1,11 @@
 package service
 
+import (
+	"fmt"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
 type PasswordService interface {
 	HashPassword(password string) (string, error)
 	VerifyPassword(password string, hash string) bool
@@ -12,7 +18,11 @@ func NewPasswordService() PasswordService {
 }
 
 func (p *passwordServiceImpl) HashPassword(password string) (string, error) {
-	panic("not implemented") // TODO: Implement
+	hashPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return "", fmt.Errorf("Failed to hash password: %w", err)
+	}
+	return string(hashPassword), nil
 }
 
 func (p *passwordServiceImpl) VerifyPassword(password string, hash string) bool {
