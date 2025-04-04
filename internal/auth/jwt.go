8e119a2876ba6abd6364efe278b16f73b4e4d7a1
Abhishek2010DevSingh <Abhishekkumar2010dev@gmail.com>
2025-04-04 17:18:40 +0530

    auth: EncodeJwtToken (renamed)

--- internal/auth/jwt.go
+++ internal/auth/jwt.go
@@ -6,7 +6,7 @@ import (
 	"github.com/golang-jwt/jwt/v5"
 )
 
-func GenerateJwtToken(claims Claims, secret []byte) (string, error) {
+func EncodeJwtToken(claims Claims, secret []byte) (string, error) {
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
 	return token.SignedString(secret)
 }
