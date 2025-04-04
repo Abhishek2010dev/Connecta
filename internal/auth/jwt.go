e58a71d2d34d8a2d2c6f1f7f27362b9a6184b973
Abhishek2010DevSingh <Abhishekkumar2010dev@gmail.com>
2025-04-04 17:17:49 +0530

    auth: DecodeJwtToken

--- internal/auth/jwt.go
+++ internal/auth/jwt.go
@@ -1,10 +1,40 @@
 package auth
 
-import "github.com/golang-jwt/jwt/v5"
+import (
+	"errors"
 
-func GenerateJwt(claims Claims, secret string) (string, error) {
+	"github.com/golang-jwt/jwt/v5"
+)
+
+func GenerateJwtToken(claims Claims, secret []byte) (string, error) {
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
 	return token.SignedString(secret)
 }
 
-func ValidateJwt(token string) (Claims, error) {}
+func DecodeJwtToken(tokenStr string, secret []byte) (*Claims, error) {
+	var claims Claims
+	token, err := jwt.ParseWithClaims(tokenStr, &claims, func(t *jwt.Token) (interface{}, error) {
+		return secret, nil
+	})
+	if err != nil {
+		var tokenValidationErr error
+		switch {
+		case errors.Is(err, jwt.ErrTokenExpired):
+			tokenValidationErr = ErrTokenExpired
+		case errors.Is(err, jwt.ErrInvalidType):
+			tokenValidationErr = ErrInvalidTokenFormat
+		case errors.Is(err, jwt.ErrTokenSignatureInvalid):
+			tokenValidationErr = ErrInvalidTokenSignature
+		default:
+			tokenValidationErr = ErrTokenValidationFailed
+		}
+
+		return nil, tokenValidationErr
+	}
+
+	if !token.Valid {
+		return nil, ErrTokenValidationFailed
+	}
+
+	return &claims, nil
+}
