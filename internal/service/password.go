3d3ae06a2de014a34e6be2ff5b667891af7a5dba
Abhishek2010DevSingh <Abhishekkumar2010dev@gmail.com>
2025-04-04 18:51:47 +0530

    service: PasswordSerivceImpl New function

--- internal/service/password.go
+++ internal/service/password.go
@@ -4,3 +4,17 @@ type PasswordService interface {
 	HashPassword(password string) (string, error)
 	VerifyPassword(password string, hash string) bool
 }
+
+type passwordServiceImpl struct{}
+
+func NewPasswordService() PasswordService {
+	return &passwordServiceImpl{}
+}
+
+func (p *passwordServiceImpl) HashPassword(password string) (string, error) {
+	panic("not implemented") // TODO: Implement
+}
+
+func (p *passwordServiceImpl) VerifyPassword(password string, hash string) bool {
+	panic("not implemented") // TODO: Implement
+}
