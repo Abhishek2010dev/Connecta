bfa1818e46fd80b7d7d3d7bd4caff578f1fbbfee
Abhishek2010DevSingh <Abhishekkumar2010dev@gmail.com>
2025-04-04 16:22:27 +0530

    auth: Claims struct with new function

--- internal/auth/claims.go
+++ internal/auth/claims.go
@@ -1,8 +1,22 @@
 package auth
 
-import "github.com/golang-jwt/jwt/v5"
+import (
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+)
 
 type Claims struct {
 	UserId int64 `json:"user_id"`
 	jwt.RegisteredClaims
+}
+
+func NewClaims(userID int64, duration time.Duration) Claims {
+	return Claims{
+		UserId: userID,
+		RegisteredClaims: jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(duration)),
+			IssuedAt:  jwt.NewNumericDate(time.Now()),
+		},
+	}
 }
