22bc827c6491db643a1bae0197badfe1d1b96a26
Abhishek2010DevSingh <Abhishekkumar2010dev@gmail.com>
2025-04-05 18:29:42 +0530

    service: Session impl

--- internal/service/session.go
+++ internal/service/session.go
@@ -2,9 +2,26 @@ package service
 
 import (
 	"github.com/Abhishek2010dev/Connecta/internal/dto"
+	"github.com/Abhishek2010dev/Connecta/internal/repository"
 )
 
 type Session interface {
 	GenerateToken(userId int64) (string, error)
 	ValidateToken(token string) (*dto.AuthPaylaod, error)
+}
+
+type sessionServiceImpl struct {
+	repo *repository.Session
+}
+
+func NewSession(repo *repository.Session) Session {
+	return &sessionServiceImpl{repo}
+}
+
+func (s *sessionServiceImpl) GenerateToken(userId int64) (string, error) {
+	panic("not implemented") // TODO: Implement
+}
+
+func (s *sessionServiceImpl) ValidateToken(token string) (*dto.AuthPaylaod, error) {
+	panic("not implemented") // TODO: Implement
 }
