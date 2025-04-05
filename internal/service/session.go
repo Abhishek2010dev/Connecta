deebf385197d2e9ee350e3f6a59c62069c5534dd
Abhishek2010DevSingh <Abhishekkumar2010dev@gmail.com>
2025-04-05 18:45:34 +0530

    service/session: GenerateToken method

--- internal/service/session.go
+++ internal/service/session.go
@@ -1,27 +1,52 @@
 package service
 
 import (
+	"crypto/rand"
+	"crypto/sha256"
+	"encoding/base64"
+	"encoding/hex"
+	"time"
+
 	"github.com/Abhishek2010dev/Connecta/internal/dto"
 	"github.com/Abhishek2010dev/Connecta/internal/repository"
 )
 
+const (
+	Day             = 24 * time.Hour
+	SessionDuration = 30 * Day
+	RenewBefore     = 15 * Day
+)
+
 type Session interface {
-	GenerateToken(userId int64) (string, error)
+	GenerateToken(userID int64) (string, error)
 	ValidateToken(token string) (*dto.AuthPaylaod, error)
 }
 
 type sessionServiceImpl struct {
-	repo *repository.Session
+	repo repository.Session
 }
 
-func NewSession(repo *repository.Session) Session {
+func NewSession(repo repository.Session) Session {
 	return &sessionServiceImpl{repo}
 }
 
-func (s *sessionServiceImpl) GenerateToken(userId int64) (string, error) {
-	panic("not implemented") // TODO: Implement
+func (s *sessionServiceImpl) GenerateToken(userID int64) (string, error) {
+	bytes := make([]byte, 18)
+	if _, err := rand.Read(bytes); err != nil {
+		return "", err
+	}
+	token := base64.RawURLEncoding.EncodeToString(bytes)
+	if _, err := s.repo.Create(hashToken(token), userID, time.Now().Add(SessionDuration)); err != nil {
+		return "", err
+	}
+	return token, nil
 }
 
 func (s *sessionServiceImpl) ValidateToken(token string) (*dto.AuthPaylaod, error) {
 	panic("not implemented") // TODO: Implement
+}
+
+func hashToken(token string) string {
+	hash := sha256.Sum256([]byte(token))
+	return hex.EncodeToString(hash[:])
 }
