00c4ef44afe138b003a39d410d8299877e98f18e
Abhishek2010DevSingh <Abhishekkumar2010dev@gmail.com>
2025-04-05 15:41:27 +0530

    migrate: cascade on delete for session table

--- migrations/000002_session_table.up.sql
+++ migrations/000002_session_table.up.sql
@@ -1,5 +1,7 @@
 CREATE TABLE IF NOT EXISTS session (
 	id TEXT PRIMARY KEY,
-	user_id INT REFERENCES users(id) NOT NULL,
-	expires_at TIMESTAMP NOT NULL
-)
+	user_id INT NOT NULL,
+	expires_at TIMESTAMP NOT NULL,
+	FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+);
+
