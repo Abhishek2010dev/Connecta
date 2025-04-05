06247d807105bb8ad66a9e68c6ffe2d565fe82ae
Abhishek2010DevSingh <Abhishekkumar2010dev@gmail.com>
2025-04-05 15:31:02 +0530

    repository: new session repo

--- internal/repository/session.go
+++ internal/repository/session.go
@@ -1,6 +1,7 @@
 package repository
 
 import (
+	"database/sql"
 	"time"
 
 	"github.com/Abhishek2010dev/Connecta/internal/models"
@@ -8,8 +9,32 @@ import (
 
 // NOTE: I am returning sessionId in same function for just check
 type SessionRepository interface {
-	Create(token string, userID int64) (string, error)
+	Create(tokenHash string, userID int64, expiresAt time.Time) (string, error)
 	FindByIDWithUsername(sessionID string) (*models.Session, string, error)
 	DeleteByID(sessionID string) (string, error)
 	UpdateExpiration(sessionID string, newExpiration time.Time) (string, error)
+}
+
+type sessionRepoImpl struct {
+	db *sql.DB
+}
+
+func NewSessionRepository(db *sql.DB) SessionRepository {
+	return &sessionRepoImpl{db}
+}
+
+func (s *sessionRepoImpl) Create(tokenHash string, userID int64, expiresAt time.Time) (string, error) {
+	panic("")
+}
+
+func (s *sessionRepoImpl) FindByIDWithUsername(sessionID string) (*models.Session, string, error) {
+	panic("not implemented") // TODO: Implement
+}
+
+func (s *sessionRepoImpl) DeleteByID(sessionID string) (string, error) {
+	panic("not implemented") // TODO: Implement
+}
+
+func (s *sessionRepoImpl) UpdateExpiration(sessionID string, newExpiration time.Time) (string, error) {
+	panic("not implemented") // TODO: Implement
 }
