65cae4e7e15aedb229dc8ef3ccbc9589110f38da
Abhishek2010dev <abhishekkumar2010dev@gmail.com>
2025-04-14 15:33:50 +0000

    home page

---
+++ internal/handler/home.go
@@ -0,0 +1,19 @@
+package handler
+
+import (
+	"net/http"
+
+	"github.com/Abhishek2010dev/Connecta/internal/renderer"
+)
+
+type HomeHandler struct {
+	renderer renderer.Renderer
+}
+
+func NewHomeHandler(renderer renderer.Renderer) *HomeHandler {
+	return &HomeHandler{renderer}
+}
+
+func (h *HomeHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	h.renderer.Render(w, nil, "layout.html", "home.html")
+}
--- internal/server/routes.go
+++ internal/server/routes.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/Abhishek2010dev/Connecta/internal/handler"
 	"github.com/Abhishek2010dev/Connecta/internal/middleware"
+	"github.com/Abhishek2010dev/Connecta/internal/renderer"
 )
 
 func (s *Server) RegisterRoutes() http.Handler {
@@ -12,9 +13,10 @@ func (s *Server) RegisterRoutes() http.Handler {
 		middleware.Logging,
 	)
 	router := http.NewServeMux()
+	renderer := renderer.New("templates")
 
-	router.HandleFunc("/", handler.HomePage)
 	router.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+	router.Handle("/", handler.NewHomeHandler(renderer))
 
 	return stack(router)
 }
