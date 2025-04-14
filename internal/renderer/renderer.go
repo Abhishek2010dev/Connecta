ebf0a0b6502979cb4eb2781aa7c4ef4d669dd678
Abhishek2010dev <abhishekkumar2010dev@gmail.com>
2025-04-14 15:24:03 +0000

    renderer struct impl

--- internal/renderer/renderer.go
+++ internal/renderer/renderer.go
@@ -1,7 +1,9 @@
 package renderer
 
 import (
+	"log"
 	"net/http"
+	"path/filepath"
 	"sync"
 	"text/template"
 )
@@ -21,5 +23,34 @@ func New() Renderer {
 }
 
 func (t *templateRenderer) Render(w http.ResponseWriter, data any, templates ...string) {
-	panic("not implemented") // TODO: Implement
+	key := filepath.Join(templates...)
+	t.mu.RLock()
+	tmpl, found := t.cache[key]
+	t.mu.RUnlock()
+
+	if !found {
+		paths := make([]string, len(templates))
+		for i, templatePath := range templates {
+			paths[i] = filepath.Join(t.dirName, templatePath)
+		}
+
+		var err error
+		tmpl, err := template.ParseFiles(paths...)
+		if err != nil {
+			log.Printf("template parsing error: %v", err)
+			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+			return
+		}
+
+		t.mu.Lock()
+		t.cache[key] = tmpl
+		t.mu.Unlock()
+	}
+
+	err := tmpl.Execute(w, data)
+	if err != nil {
+		log.Printf("template execution error: %v", err)
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+	}
+
 }
