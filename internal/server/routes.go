fe11ab38aac586c4841c1e0a3ce024a0f67a3d12
Abhishek2010dev <abhishekkumar2010dev@gmail.com>
2025-04-18 11:40:02 +0530

    chi middleware

--- internal/server/routes.go
+++ internal/server/routes.go
@@ -4,10 +4,16 @@ import (
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 func (s *Server) RegisterRoutes() http.Handler {
 	router := chi.NewRouter()
+
+	router.Use(middleware.RequestID)
+	router.Use(middleware.RealIP)
+	router.Use(middleware.Logger)
+	router.Use(middleware.Recoverer)
 
 	fs := http.FileServer(http.Dir("static"))
 	router.Handle("/static/*", http.StripPrefix("/static", fs))
